baf2bc8e7f3305f43139e796f1ad8b2ece796cca
RedRhin0 <nosorog76rus@ro.ru>
2022-11-20 16:58:19 +0300

    Removed extra print

--- task5/task5.py
+++ task5/task5.py
@@ -66,8 +66,3 @@ def task(jsonString1, jsonString2):
 
     return (core)
 
-
-a = json.dumps(["1", ["2", "3"], "4", ["5", "6", "7"], "8", "9", "10"])
-b = json.dumps([["1", "2"], ["3", "4", "5"], "6", "7", "9", ["8", "10"]])
-
-print(task(a, b))
