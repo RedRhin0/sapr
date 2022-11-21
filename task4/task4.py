9802c02d60e0bf8122ba6b080b3688ee9d24407f
RedRhin0 <nosorog76rus@ro.ru>
2022-11-21 23:02:18 +0300

    Upload task4

---
+++ task4/task4.py
@@ -0,0 +1,11 @@
+from io import StringIO
+import csv
+
+
+def task(csvString):
+    f = StringIO(csvString)
+    reader = csv.reader(f, delimiter=',')
+    out = []
+    for row in reader:
+        out.append(row)
+    return out
