21aa1d8f4e71d377ad24f173f65ce796bae5518d
RedRhin0 <nosorog76rus@ro.ru>
2022-11-21 23:47:45 +0300

    entropy task

--- task4/task4.py
+++ task4/task4.py
@@ -1,11 +1,84 @@
 from io import StringIO
 import csv
+import math
+
+
+def getNodeList(graph):
+    nodeMap = {}
+    for [par, ch] in graph:
+        nodeMap[par] = True
+        nodeMap[ch] = True
+    return list(nodeMap.keys())
+
+
+def getChilds(node, graph):
+    childs = []
+    for [par, ch] in graph:
+        if par == node:
+            childs.append(ch)
+    return childs
+
+
+def getParents(node, graph):
+    parents = []
+    for [par, ch] in graph:
+        if ch == node:
+            parents.append(par)
+    return parents
+
+
+def getAncestors(node, graph):
+    ancestors = []
+    parents = getParents(node, graph)
+    for par in parents:
+        grandParents = getParents(par, graph)
+        if len(grandParents) > 0:
+            ancestors.extend(grandParents)
+            ancestors.extend(getAncestors(par, graph))
+    return ancestors
+
+
+def getDescendants(node, graph):
+    descendants = []
+    children = getChilds(node, graph)
+    for child in children:
+        grandChildren = getChilds(child, graph)
+        if len(grandChildren) > 0:
+            descendants.extend(grandChildren)
+            descendants.extend(getAncestors(child, graph))
+    return descendants
+
+
+def getNeighbours(node, graph):
+    parents = getParents(node, graph)
+    neighbours = []
+    for parent in parents:
+        children = getChilds(parent, graph)
+        children.remove(node)
+        neighbours.extend(children)
+    return neighbours
 
 
 def task(csvString):
     f = StringIO(csvString)
     reader = csv.reader(f, delimiter=',')
-    out = []
+    graph = []
     for row in reader:
-        out.append(row)
-    return out
+        graph.append(row)
+    nodeList = getNodeList(graph)
+    graphStats = []
+    for node in nodeList:
+        graphStats.append([node, [len(getChilds(node, graph)), len(getParents(node, graph)), len(
+            getDescendants(node, graph)), len(getAncestors(node, graph)), len(getNeighbours(node, graph))]])
+
+    entropy = 0
+    n = len(graphStats)
+    for [node, nodeStats] in graphStats:
+        in_sum = 0
+        for i in nodeStats:
+            p = i/(n-1)
+            if p > 0:
+                in_sum += p*math.log(p, 2)
+        entropy += in_sum
+
+    return -entropy
